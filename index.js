0ccaa781b3092766ebae0d304aba2f5ce9d0d2ff
Shaw <shaw.s.li@gmail.com>
2017-07-22 23:18:46 +0000

    significiant progress in defining operations

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 /*global $*/
 
 $(document).ready(function() {
-<<<<<<< HEAD
     var period = false;
+    var multiply = "*";
+    var divide = "/";
+    var add = "+";
+    var subtract = "-";
     
     function periodInsert(data){
         if (!period){
@@ -53,6 +56,50 @@ $(document).ready(function() {
         }
     }
     
+    function returnArrayofOperationPosition(string) {
+        var outputArray = [];
+        for (var i = 0; i < string.length; i++) {
+            if (string.charAt(i) !== "." && isNaN(string.charAt(i)) && i !== string.length - 1) {
+                outputArray.push(i);
+            }
+        }
+        return outputArray;
+    }
+    
+    
+    function returnFinalArray(string) {
+        var outputArray = [];
+        // this if statement removes the last character if it is an operation or period
+        if (isNaN(string.charAt(string.length - 1))) {
+            string = string.slice(0, -1);
+        }
+        //call function to create array of position where the operation is located
+        var tempArray = returnArrayofOperationPosition(string);
+        
+        //creates final array with all numbers and operations
+        outputArray.push(Number(string.slice(0, tempArray[0])));
+        for (var i = 0; i < tempArray.length; i++) {
+            if (i % 2 == 0) {
+                outputArray.push(string.slice(tempArray[i], tempArray[i] + 1));
+                outputArray.push(Number(string.slice(tempArray[i] + 1, tempArray[i + 1])));
+            }
+            else {
+                outputArray.push(string.slice(tempArray[i], tempArray[i] + 1));
+                outputArray.push(Number(string.slice(tempArray[i] + 1, tempArray[i + 1])));
+            }
+        }
+        console.log(outputArray);
+        return outputArray;
+    }
+    
+    function executeOrderOfOperations(arg, arg2){
+        arg.splice(arg.indexOf(arg2)-1),3,arg[arg.indexOf(arg2)-1]*arg[arg.indexOf(arg2)+1];    
+        return arg;
+    }
+    
+    
+    
+    
     
     $("#clear").click(function(){
        setZero();
@@ -75,14 +122,42 @@ $(document).ready(function() {
        str = str.slice(0, -1);
        console.log(str);
        removeOne(str);
-       
     });
     
     
-    
-    
-    
-=======
+    $("#equal").click(function() {
+        var data = $("#display").html();
+        var operators = {
+            "multiply": 0,
+            "divide":   0,
+            "add":      0,
+            "subtract": 0
+        };
+        
+        var operatorPositionArray = returnArrayofOperationPosition(data);
+        for (var i = 0; i<operatorPositionArray.length; i++){
+            switch(data[operatorPositionArray[i]]) {
+                case multiply:
+                    operators.multiply++;
+                    break;
+                case divide:
+                    operators.divide++;
+                    break;
+                case add:
+                    operators.add++;
+                    break;
+                case subtract:
+                    operators.subtract++;
+                    break;
+            }
+            
+            
+            
+            
+        };
+        
+        executeOrderOfOperations(returnArrayofOperationPosition(data),multiply);
+    });
  
     
 });
